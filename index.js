6d5402a16b8fe29f8cfb2c7bd22f2ee0655ea65f
Hemlal Dulal <hemlaldulal.986@gmail.com>
2023-12-15 20:38:16 +0545

    feat: Created server for GraphQL

---
+++ index.js
@@ -0,0 +1,11 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+//* Server setup
+const server = new ApolloServer({});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: 4000`);
